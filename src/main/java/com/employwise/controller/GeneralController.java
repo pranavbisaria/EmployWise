2524a37918c4141187579d74bc0175cfe0cb7981
Pranav Bisaria <pranavbisariya29@gmail.com>
2023-12-19 21:46:33 +0530

    update: github url

--- src/main/java/com/employwise/controller/GeneralController.java
+++ src/main/java/com/employwise/controller/GeneralController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class GeneralController {
     @GetMapping("/")
     public ResponseEntity<?> status() {
-        return new ResponseEntity<>("<h1>Welcome!!</h1><a href = \"/swagger-ui/index.html\">API Documentation</a><br><a href = \"/swagger-ui/index.html\">GitHub Repository</a><p id=\"bottom-section\">ThankYou!!!<Br><b>Regards,<br>Pranav Bisaria<br>Phone Number: +918287027446<br>Email: <a href=\"mailto: pranavbisariya29@gmail.com\">pranavbisariya29@gmail.com</a></b></p>", HttpStatus.OK);
+        return new ResponseEntity<>("<h1>Welcome!!</h1><a href = \"/swagger-ui/index.html\">API Documentation</a><br><a href = \"https://github.com/pranavbisaria/employ-wise\">GitHub Repository</a><p id=\"bottom-section\">ThankYou!!!<Br><b>Regards,<br>Pranav Bisaria<br>Phone Number: +918287027446<br>Email: <a href=\"mailto: pranavbisariya29@gmail.com\">pranavbisariya29@gmail.com</a></b></p>", HttpStatus.OK);
     }
 }
